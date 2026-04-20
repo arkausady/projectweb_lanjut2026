5d5961b45f48541e696d5b3c093089d177efbdbd
arkawicak <arkawicak@gmail.com>
2026-04-20 10:24:38 +0700

    second_commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KontakController;
+use App\Http\Controllers\ProfilController;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/kontak', [KontakController::class, 'index']);
+Route::get('/profil', [ProfilController::class, 'index']);
